128c5840df27ca351fb61e2e5d9ff11834071969
sheerapi <crystilagoslagos945@gmail.com>
2024-02-03 20:57:31 -0300

    Added website

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    skipTrailingSlashRedirect: true,
-    images: {
-        unoptimized: true,
-      },
+
     distDir: 'out'
 };
 
