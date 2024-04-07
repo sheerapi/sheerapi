af9f6ded196fa78d7264917541259aa448e58d22
sheerapi <crystilagoslagos945@gmail.com>
2024-04-07 15:13:29 -0400

    Added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/ui/theme-provider"
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,12 @@ export default function RootLayout({
 			<head>
 				<meta name="google-site-verification" content="sG8Oh9UyRJEqvc9vO80nBgZtD2UWlGDYiw3qTpIJLBk" />
 			</head>
-			<body className={inter.className}><ThemeProvider attribute="class"
-				defaultTheme="dark">{children}</ThemeProvider></body>
+			<body className={inter.className}>
+				<SpeedInsights />
+				<ThemeProvider attribute="class" defaultTheme="dark">
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
