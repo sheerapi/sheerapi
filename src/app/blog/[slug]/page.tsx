c3c37591eb8e563bed099a5916632e35f2177a25
sheerapi <crystilagoslagos945@gmail.com>
2024-04-13 14:08:35 -0400

    ahf

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -19,7 +19,7 @@ export default async function Page(params: { params: { slug: string } }) {
     }
 
     const date = new Date(parseInt(post.date));
-    const dateString = `${date.getDate().toString().padStart(2, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear().toString().padStart(4, '0')}, ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+    const dateString = `${date.getDate().toString().padStart(2, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear().toString().padStart(4, '0')}, ${(date.getHours() - 4).toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
 
     return (
         <main className="flex justify-start items-start h-full flex-col gap-5">
